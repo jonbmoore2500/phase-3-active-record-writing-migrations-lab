ed29273c930849cae69fcbeb9df6feefcb69a229
Jonathan <jonathanmoorevla@gmail.com>
2022-12-09 11:27:20 -0600

    Create migrations, pass tests

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+      # make sure to end the "do" block!
+    end
   end
 end
